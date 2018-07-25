52c533148af1bc785740ad1f0c8f974b759537df
whynotski <laurawynosky@gmail.com>
2018-07-25 16:21:12 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,22 @@
+require 'pry'
+
 class Owner
-  # code goes here
-end
+
+    @@all=[]
+
+    attr_accessor :name, :pets, :species
+
+
+  def initialize (pets)
+    @pets={fishes: [], cats: [], dogs: []}
+    @species=species
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+
+end# end of class Owner
