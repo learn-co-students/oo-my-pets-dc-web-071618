7b9d1a0cbedbaa49b8bb471173ecc249495e51c6
whynotski <laurawynosky@gmail.com>
2018-07-25 17:38:51 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -4,8 +4,8 @@ class Owner
 
     @@all=[]
 
-    attr_accessor :name, :pets, :species
-
+    attr_accessor :name, :pet
+    attr_accessor :species
 
   def initialize (pets)
     @pets={fishes: [], cats: [], dogs: []}
@@ -17,6 +17,38 @@ class Owner
     @@all
   end
 
+def owner.count
 
+def owner.reset
+
+def say_species
+end
+
+def name
+end
+
+def buy_fish
+end
+
+def buy_cat
+end
+
+def buy_dog
+end
+
+def walk_dogs
+end
+
+def play_with_cats
+end
+
+def feed_fish
+end
+
+def list_pets
+end
+
+def sell_pets
+end
 
 end# end of class Owner
